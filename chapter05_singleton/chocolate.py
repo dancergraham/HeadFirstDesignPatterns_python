c275a968b30c8dbf08b60081fc715bb99a965554
Graham Knapp <graham.knapp@cstb.fr>
2021-06-20 19:57:49 +0200

    Singleton readme

--- chapter05_singleton/chocolate.py
+++ chapter05_singleton/chocolate.py
@@ -1,19 +1,21 @@
 class SingletonError(Exception):
     pass
 
+
 class Singleton(type):
     _instances = {}
+
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
-        
+
 
 class ChocolateBoiler(metaclass=Singleton):
     def __init__(self) -> None:
         self.empty = True
         self.boiled = False
-    
+
     def fill(self):
         if not self.empty:
             raise SingletonError
@@ -47,8 +49,5 @@ def chocolate_controller():
     print(boiler2 is boiler)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     chocolate_controller()
-
-    
---
+++ chapter05_singleton/readme.md
@@ -0,0 +1,5 @@
+# Chapter 5: Singleton design pattern
+
+**Singleton**: ensures a class has only one instance, and provides a global point of access to it.
+
+Based this [stackoverflow post](https://stackoverflow.com/questions/6760685/creating-a-singleton-in-python?noredirect=1&lq=1).
