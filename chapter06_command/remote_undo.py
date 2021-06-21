495ff6ab783aeea81ae5b3149c546ffc8761d785
Graham Knapp <graham.knapp@cstb.fr>
2021-06-21 17:22:12 +0200

    Implement Light object

--- chapter06_command/remote_undo.py
+++ chapter06_command/remote_undo.py
@@ -39,3 +39,28 @@ class LightOffCommand(Command):
  
     def undo(self):
         self.light.dim(self.level)
+
+
+class Light():
+    def __init__(self, location:str):
+        self.location = location
+
+    def on(self):
+        self.level = 100
+        print("Light is on")
+
+    def off(self):
+        self.level = 0
+        print("Light is off")
+
+    def dim(self, level):
+        self.level = level
+        if level == 0:
+            self.off()
+        else:
+            print(f"Light is dimmed to {self.level}%")
+    
+    def get_level(self):
+        return self.level
+
+
